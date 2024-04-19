cf8715b2b6f9e3675e6636a3cb48d776e6db2280
neoncodes <41237443+charleywolf@users.noreply.github.com>
2024-04-19 18:56:09 -0400

    updated quick help for how to paste grades guide

--- src/app/help/grades/page.tsx
+++ src/app/help/grades/page.tsx
@@ -9,19 +9,24 @@ export default function gradesHelp() {
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
         <HelpColumn
           src="diagram1.png"
-          alt="Image of Google Classroom"
+          alt="Google Classroom"
           caption='Navigate to your class&apos;s Google Classroom page and click on
         "Classwork" in the topbar.'
         />
         <HelpColumn
           src="diagram2.png"
-          alt="Image of Google Classroom: Classwork page"
+          alt="Google Classroom: Classwork page"
           caption='Click on the "View your work" button located under the topbar.'
         />
         <HelpColumn
           src="diagram3.png"
-          alt="Image of Google Classroom: View your work section"
-          caption='Select all your grades as displayed and copy them using CTRL-C (or CMD-C on MacOS). Finally, return to our website paste in your grades under the label "Google Classroom Grades"!'
+          alt="Google Classroom: View your work section"
+          caption="Select all your grades as displayed and copy them using CTRL-C (or CMD-C on MacOS)."
+        />
+        <HelpColumn
+          src="diagram4.png"
+          alt="Google Classroom Grade Viewer"
+          caption='Return to our website and paste in your grades under "Google Classroom Grades". Finally, click on "Calculate Average" and view the calculated grade in the top-right.'
         />
       </div>
     </div>
