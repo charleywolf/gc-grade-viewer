64960962ae0eece5c74cfa1a9beeb461ecb276ba
Charley <41237443+charleywolf@users.noreply.github.com>
2024-04-30 14:23:23 -0400

    Update 513.ts for accurqacy

--- src/lib/gradingSystems/513/513.ts
+++ src/lib/gradingSystems/513/513.ts
@@ -12,7 +12,7 @@ export default function System513(grades: Grade[]): number {
   };
 
   for (const grade of grades) {
-    if (grade.possible > 20) {
+    if (grade.possible > 40) {
       testGrade.earned += grade.earned;
       testGrade.possible += grade.possible;
     } else {
