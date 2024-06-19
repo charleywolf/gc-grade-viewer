9de6aa7296540681becce8618ce68579fbbd2991
neoncodes <41237443+charleywolf@users.noreply.github.com>
2024-06-19 18:59:22 -0400

    increased load times by using edge for authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,31 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
+import { getSession } from "@auth0/nextjs-auth0/edge";
 import { kv } from "@vercel/kv";
 
 export async function middleware(request: NextRequest) {
-  const ip = request.ip;
-  const buf = await crypto.subtle.digest(
-    "SHA-256",
-    new TextEncoder().encode(ip)
-  );
+  if (request.nextUrl.pathname.startsWith("/premium")) {
+    const user = await getSession();
+    if (user === null)
+      return NextResponse.redirect(new URL("/api/auth/login", request.url));
+  } else {
+    const ip = request.ip;
+    const buf = await crypto.subtle.digest(
+      "SHA-256",
+      new TextEncoder().encode(ip)
+    );
 
-  const hash = Array.from(new Uint8Array(buf))
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join("");
+    const hash = Array.from(new Uint8Array(buf))
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
 
-  const isNew = await kv.set(["deduplicate", hash].join(":"), true, {
-    nx: true,
-    ex: 60 * 60, // 1 hour expiry
-  });
+    const isNew = await kv.set(["deduplicate", hash].join(":"), true, {
+      nx: true,
+      ex: 60 * 60, // 1 hour expiry
+    });
 
-  if (isNew === "OK") {
-    await kv.incr("siteviews");
+    if (isNew === "OK") {
+      await kv.incr("siteviews");
+    }
   }
 }
 
